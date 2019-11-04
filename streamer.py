36c000ce92c8dbb8c7bfcb59a76aa1f279dfd829
SuyashMore <suyash.21.more@gmail.com>
2019-11-04 19:07:29 +0530

    Added Recording to recVideos Folder

--- streamer.py
+++ streamer.py
@@ -1,16 +1,40 @@
 from customeStreamer import Streamer
 import cv2
+from datetime import datetime
+import time
+
+VIDEO_RECORD_OUTFOLDER = "recVideos/"
 
 streamer = Streamer(port=100,frame_rate=60)
 
-# video_capture = cv2.VideoCapture("http://192.168.0.100:4747/video")
+video_capture = cv2.VideoCapture("http://192.168.0.102:4747/video")
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')  # cv2.VideoWriter_fourcc() does not exist
+video_writer = None
 
 while True:
-    # _, frame = video_capture.read()
+    _, frame = video_capture.read()
+
+    if streamer.isRecording:
+        if video_writer is None:
+            dt_string = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+            # dt_string = str(time.time())
+            outFile = VIDEO_RECORD_OUTFOLDER+dt_string + ".avi"
+            print(f"Outfile:{outFile}")
+            w = video_capture.get(cv2.CAP_PROP_FRAME_WIDTH);
+            h = video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT);
+            video_writer=cv2.VideoWriter(outFile,fourcc,15,(int(w),int(h)))
+        print("Writing Frame !!")
+        video_writer.write(frame)
+    else:
+        if video_writer is not None:
+            print("Stoppping Recording !!")
+            video_writer.release()
+            video_writer = None
 
     # Process the Frame
 
-    # streamer.update_frame(frame)
+    streamer.update_frame(frame)
 
     if not streamer.is_streaming:
         streamer.start_streaming()
